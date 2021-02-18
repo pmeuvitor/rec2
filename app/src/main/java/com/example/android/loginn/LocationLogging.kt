dab419614f930f04a5e31f4967890a6e52e800dc
pmeuvitor <inestuxa@hotmail.com>
2021-02-18 00:05:56 +0000

    ol

---
+++ app/src/main/java/com/example/android/loginn/LocationLogging.kt
@@ -0,0 +1,9 @@
+package com.example.android.loginn
+
+import com.google.firebase.database.IgnoreExtraProperties
+@IgnoreExtraProperties
+
+data class LocationLogging(
+    var Latitude: Double? = 0.0,
+    var Longitude: Double? = 0.0
+)
